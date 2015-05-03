98a618dccf621d567e6ee5776372cbca4837e4a8
bryce meyer <bryce@Rupert.local>
2015-05-03 02:58:42 -0500

    added basic wordpress orm

---
+++ classes/ModelInterface.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 5/3/15
+ * Time: 2:55 AM
+ */
+
+namespace MagicTimeline;
+
+/**
+ * Interface for ORM models.
+ *
+ * @author Brandon Wamboldt <brandon.wamboldt@gmail.com>
+ */
+interface ModelInterface
+{
+    /**
+     * Overwrite this in your concrete class. Returns the table name used to
+     * store models of this class.
+     *
+     * @return string
+     */
+    public static function get_table();
+
+    /**
+     * Get an array of fields to search during a search query.
+     *
+     * @return array
+     */
+    public static function get_searchable_fields();
+}
