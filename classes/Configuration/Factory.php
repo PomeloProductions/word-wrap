c277daf6adb6ce1c121579beafdeb6e72f68729b
bryce meyer <bryce@Rupert.local>
2015-05-03 23:30:34 -0500

    filled out function body for inflating the configuration

--- classes/Configuration/Factory.php
+++ classes/Configuration/Factory.php
@@ -23,10 +23,17 @@ class Factory {
      * @param $pluginName string the name of the plugin that we are reading the configuration for
      * @throws Exception The json file should be located at the root of the plugin, and it should be named info.json
      *                      if it isn't then we have no idea what this plugin is about
-     * @return Base We will be constructing an instance of our Base configuration and returning it
+     * @return Root We will be constructing an instance of our Root configuration and returning it
      */
     public static function inflate($pluginName) {
+        $path = get_home_path() . "wp-content/plugins/" . $pluginName . "/info.json";
+        if(!file_exists($path))
+            throw new Exception("Unable to find plugin configuration. Please make sure that you have a info.json included in the root of your plugin");
 
+        $fileContents = file_get_contents($path);
+        $json = json_decode($fileContents, true);
+
+        return new Root($json);
     }
 
     //TODO create a function to write out an instance of base to file
