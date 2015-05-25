9d59d15d68aa98cb92ea3177dd3ed13eb187a346
Bryce Meyer <bryce@axolotlinteractive.com>
2015-05-24 21:37:38 -0500

    added constructor to view

--- classes/View/View.php
+++ classes/View/View.php
@@ -9,6 +9,24 @@
 namespace WordWrap\View;
 
 
+use WordWrap\LifeCycle;
+
 class View {
 
+    /**
+     * @var LifeCycle the current running instance of the plugin
+     */
+    protected $lifeCycle;
+
+    private $templateContent;
+
+    /**
+     * @param $lifeCycle LifeCycle the current running LifeCycle
+     * @param $templateName string the name of the template we are loading
+     */
+    function __construct($lifeCycle, $templateName) {
+        $this->lifeCycle = $lifeCycle;
+
+        $this->lifeCycle->assetManager->loadAsset("html", $templateName);
+    }
 }
