2b0cd17bb9728ece613292d1839c452758175e29
Bryce Meyer <bryce@axolotlinteractive.com>
2015-09-12 20:05:16 -0500

    Setup task interface

--- classes/Admin/TaskController.php
+++ classes/Admin/TaskController.php
@@ -9,22 +9,58 @@
 namespace WordWrap\Admin;
 
 
+use WordWrap\Configuration\AdminMenu;
 use WordWrap\Configuration\Task;
+use WordWrap\LifeCycle;
+use WordWrap\View\View;
 
 abstract class TaskController {
 
     /**
-     * @var Task the configuration of this task
+     * @var LifeCycle the current running instance of LifeCycle
      */
-    public $task;
+    protected $lifeCycle;
 
     /**
+     * @var AdminController the configuration of this task
+     */
+    protected $adminController;
+
+    /**
+     * @var string the name of this page
+     */
+    protected $pageName;
+
+    /**
+     * @var Task the task that this controller is represented by
+     */
+    protected $task;
+
+    /**
+     * @param LifeCycle $lifeCycle the current running life cycle of this plugin
+     * @param AdminController $adminController the configuration for this controller
      * @param Task $task the configuration for this controller
      */
-    public function __construct(Task $task) {
+    public function __construct(LifeCycle $lifeCycle, AdminController $adminController, Task $task = null) {
+
+        $this->lifeCycle = $lifeCycle;
+        $this->adminController = $adminController;
         $this->task = $task;
 
+        $this->setup();
+    }
 
+    /**
+     * override this to setup anything that needs to be done before
+     */
+    public abstract function setup();
+
+    /**
+     * @return string the content of the page to render
+     */
+    public final function renderPageContent() {
+
+        //TODO build task page content
     }
 
     /**
---
+++ classes/Admin/Tasks/AvailableTasks.php
@@ -0,0 +1,14 @@
+<?php
+namespace WordWrap\Admin\Task;
+use WordWrap\Admin\TaskController;
+
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 9/12/15
+ * Time: 1:01 PM
+ */
+class AvailableTasks extends TaskController {
+
+
+}
