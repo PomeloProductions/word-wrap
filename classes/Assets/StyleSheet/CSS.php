fb268ae337dfbadc6f235e516f99ed8e4c071f4c
Bryce Meyer <bryce13950@gmail.com>
2015-09-17 01:01:32 -0500

    added css asset wrapper

---
+++ classes/Assets/StyleSheet/CSS.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WordWrap\Assets\StyleSheet;
+use WordWrap\Assets\BaseAsset;
+use WordWrap\LifeCycle;
+
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 9/17/15
+ * Time: 12:57 AM
+ */
+class CSS extends BaseAsset {
+
+    /**
+     * @param LifeCycle $lifeCycle
+     * @param string $assetName the name of the asset we are loading
+     */
+    public function __construct($lifeCycle, $assetName) {
+        parent::__construct($lifeCycle, $assetName, "css");
+    }
+}
