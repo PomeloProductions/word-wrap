148c6568c195d2d866411839cf226ae13e97ff1c
Bryce Meyer <bryce@axolotlinteractive.com>
2017-02-08 14:54:06 -0600

    added optional where for fetch ordered by

--- classes/ORM/BaseModel.php
+++ classes/ORM/BaseModel.php
@@ -383,17 +383,18 @@ abstract class BaseModel implements ModelInterface {
      *
      * @param $field string the field that we are ordering by
      * @param $direction string the direction of the field
+     * @param string $where Any additional where that we want
      * @return static[] array of objects of called class
      * todo allow deleted at override
      */
-    public static function fetchOrderedBy($field, $direction) {
+    public static function fetchOrderedBy($field, $direction, $where = '') {
 
         global $wpdb;
         $results = [];
 
         $table = static::getFullTableName();
 
-        $SQL = "SELECT * FROM `" . $table . "` WHERE `deleted_at` IS NULL ORDER BY `" . $field . "` " . $direction;
+        $SQL = "SELECT * FROM `" . $table . "` WHERE `deleted_at` IS NULL " . $where . " ORDER BY `" . $field . "` " . $direction;
 
         $rows = $wpdb->get_results($SQL);
 
